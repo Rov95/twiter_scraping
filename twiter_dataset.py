46bfb54eb52d06a10e724a5caa518809bce63a99
Rov95 <165959704+Rov95@users.noreply.github.com>
2024-04-11 13:58:27 -0500

    Add files via upload

---
+++ twiter_dataset.py
@@ -0,0 +1,33 @@
+from ntscraper import Nitter
+import pandas as pd
+
+def create_twiter_dataset(username, no_of_tweets):
+    scraper = Nitter(log_level=1, skip_instance_check=False)
+    tweets = scraper.get_tweets(username, mode="user", number=no_of_tweets)
+    data = {
+    'link':[],
+    'text':[],
+    'user':[],
+    'likes':[],
+    'quotes':[],
+    'retweets':[],
+    'comments':[]
+    }
+
+    for tweet in tweets['tweets']:
+        data['link'].append(tweet['link'])
+        data['text'].append(tweet['text'])
+        data['user'].append(tweet['user']['name'])
+        data['likes'].append(tweet['stats']['likes'])
+        data['quotes'].append(tweet['stats']['quotes'])
+        data['retweets'].append(tweet['stats']['retweets'])
+        data['comments'].append(tweet['stats']['comments'])
+
+    df = pd.DataFrame(data)
+    name = '{}.csv'.format(username)
+    df.to_csv(name)
+    return df.head()
+
+p = create_twiter_dataset('Colpensiones', 1000)
+
+print(p)
